beb3676c0a617e6049962f96c177144e0ad7e767
mfrost03 <91555247+mfrost03@users.noreply.github.com>
2021-09-28 14:29:16 -0500

    Create ItemController.cs

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController: Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public IActionResult Index(Item item)
+        {
+
+            return View("Index");
+
+        }
+        [HttpGet]
+        public IActionResult Create() 
+        {
+
+            return View("Create");
+
+        }
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+           
+            RedirectToAction("Index");
+            return View("Create");
+            
+        }
+        public IActionResult Delete(int id)
+        {
+
+            RedirectToAction("Index");
+            return View("Create");
+        }
+    }
+}
